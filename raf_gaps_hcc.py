bda62d718bc752d4ae3a414e1f21598c79584c31
richardkmi <83045076+richardkmi@users.noreply.github.com>
2022-08-16 08:51:41 -0700

    v1

--- raf_gaps_hcc.py
+++ raf_gaps_hcc.py
@@ -16,30 +16,32 @@ st.title('Hana Hou RAF Gap Logic')
 
 prev_year_dx = st.selectbox('Previous Year ICD-10:',("E1151", "E1169", "E1122"))
 curr_year_dx = st.selectbox('Current Year ICD-10 (Claims, EHR, or both):',
-  ("E1151", "Not Recorded", "E1169", "E1122", "R05"))
+  ("Not Recorded", "E1151", "E1169", "E1122", "R05"))
 
 
 
-care_gap_dx_level = prev_year_dx != curr_year_dx
+caregap_dx_level = prev_year_dx != curr_year_dx
 
 
 rp = he.profile([prev_year_dx])
 prev_hcc = get_hcc_from_icd10(prev_year_dx)
 curr_hcc = get_hcc_from_icd10(curr_year_dx)
-care_gap_hcc_level = len(set(prev_hcc).intersection(curr_hcc)) == 0
+caregap_hcc_level = len(set(prev_hcc).intersection(curr_hcc)) == 0
 
-rec_date = st.date_input("Enter dx capture date:")
-max_date = st.date_input("Enter most recent (any) dx date for patient and hcc:",
-value = datetime.date(2022, 3, 14))
+most_recent_dx = st.selectbox('Current year dx is most recent for patient and HCC',("Ignore", True, False))
 
-max_date_gap = max_date >=rec_date and (care_gap_hcc_level or care_gap_dx_level)
-
-dat = dict(prev_year_dx=[prev_year_dx],
+if most_recent_dx == "Ignore":
+  dat = dict(prev_year_dx=[prev_year_dx],
            curr_year_dx =[curr_year_dx],
-           hcc = [prev_hcc],
-           care_gap_dx_level =[care_gap_dx_level],
-           care_gap_hcc_level = [care_gap_hcc_level],
-           care_gap_dx_recent_level = max_date_gap)
+           hcc_match = not caregap_hcc_level,
+           caregap_dx =[caregap_dx_level],
+           caregap_hcc = [caregap_hcc_level])
+else:
+  dat = dict(prev_year_dx=[prev_year_dx],
+           curr_year_dx =[curr_year_dx],
+           hcc_match = not caregap_hcc_level,
+           caregap_dx =[caregap_dx_level and most_recent_dx],
+           caregap_hcc = [caregap_hcc_level and most_recent_dx])
 st.table(pd.DataFrame(dat))
 
 
